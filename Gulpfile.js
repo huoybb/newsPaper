f10fa8240c4ade0fc2b85ed0cdbfa1ce8c920450
zhaobing <zhaobing024@gmail.com>
2016-06-26 06:54:30 +0800

    重新设置 gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,30 +8,34 @@ var coffee = require('gulp-coffee');
 // var autoprefixer = require('gulp-autoprefixer');
 var notify = require('gulp-notify');
 
-var sassDir = 'app/assets/sass';
-var cssDir = 'public/css';
+var cssDir = {
+    src: 'app/assets/sass',
+    dest: 'public/css'
+};
 
-var coffeeDir = 'app/assets/coffee';
-var jsDir = 'public/js';
+var jsDir = {
+    src: 'app/assets/coffee',
+    dest:'public/js'
+};
 
 gulp.task('css',function(){
-    return gulp.src(sassDir + '/main.scss')
+    return gulp.src(cssDir.src + '/main.sass')
         .pipe(sass({style: 'compressed'}))
         // .pipe(autoprefixer('last 10 version'))
-        .pipe(gulp.dest(cssDir))
-        .pipe(notify('CSS compiled, prefixed, and minified.'));
+        .pipe(gulp.dest(cssDir.dest))
+        .pipe(notify('CSS compiled and minified.'));
 });
 
 gulp.task('js',function () {
-    return gulp.src(coffeeDir + '/**/*.coffee')
+    return gulp.src(jsDir.src + '/**/*.coffee')
         .pipe(coffee().on('error',gutil.log))
-        .pipe(gulp.dest(jsDir))
+        .pipe(gulp.dest(jsDir.dest))
         .pipe(notify('JS compiled.'));
 });
 
 gulp.task('watch',function () {
-    gulp.watch('app/assets/sass/**/*.scss',['css']);
-    gulp.watch('app/assets/coffee/**/*.coffee',['js']);
+    gulp.watch(cssDir.src + '/**/*.sass',['css']);
+    gulp.watch(jsDir.src + '/**/*.coffee',['js']);
 
 });
 
